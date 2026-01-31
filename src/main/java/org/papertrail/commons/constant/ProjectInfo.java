6b6f087f98060df1fd61b3002f0cbbb98ccfa325
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-01-31 20:36:09 +0530

    chore(release): bump project version to `2.3.0`
    - update application version in `ProjectInfo.java` from `v2.2.3` to `v2.3.0`
    - update Maven project version in `pom.xml` from `2.2.2` to `2.3.0`
    
    Signed-off-by: Egg-03 <111327101+eggy03@users.noreply.github.com>

--- src/main/java/org/papertrail/commons/constant/ProjectInfo.java
+++ src/main/java/org/papertrail/commons/constant/ProjectInfo.java
@@ -7,6 +7,6 @@ public class ProjectInfo {
 	}
 	
 	public static final String APPNAME = "PaperTrail";
-	public static final String VERSION = "v2.2.3";
+	public static final String VERSION = "v2.3.0";
 	public static final String PROJECT_ISSUE_LINK="https://github.com/eggy03/PaperTrailBot/issues";
 }
