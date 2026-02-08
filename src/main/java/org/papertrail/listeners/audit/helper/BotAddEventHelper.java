5b9788b0dd40f21490ed28ef4894556cc514f545
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-02-08 09:44:04 +0530

    refactor(audit-listeners): streamline `BotAddEventHelper` embed creation
    - remove unused `MessageEmbed` import
    - directly build and send the embed without an intermediate variable
    - adjust embed description and field names for improved clarity
    
    Signed-off-by: Egg-03 <111327101+eggy03@users.noreply.github.com>

--- src/main/java/org/papertrail/listeners/audit/helper/BotAddEventHelper.java
+++ src/main/java/org/papertrail/listeners/audit/helper/BotAddEventHelper.java
@@ -3,7 +3,6 @@ package org.papertrail.listeners.audit.helper;
 import lombok.experimental.UtilityClass;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.audit.AuditLogEntry;
-import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.guild.GuildAuditLogEntryCreateEvent;
@@ -17,30 +16,28 @@ public class BotAddEventHelper {
 
         AuditLogEntry ale = event.getEntry();
 
-        EmbedBuilder eb = new EmbedBuilder();
-        eb.setTitle("Audit Log Entry | Bot Add Event");
-
         User executor = ale.getJDA().getUserById(ale.getUserIdLong());
-        User target = ale.getJDA().getUserById(ale.getTargetIdLong());
-
         String mentionableExecutor = (executor != null ? executor.getAsMention() : ale.getUserId());
+
+        User target = ale.getJDA().getUserById(ale.getTargetIdLong());
         String mentionableTarget = (target !=null ? target.getAsMention() : ale.getTargetId());
 
-        eb.setDescription("👤 **By**: "+mentionableExecutor+"\nℹ️ A bot was added");
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setTitle("Audit Log Entry | Bot Add Event");
+        eb.setDescription("ℹ️ A bot was added by: "+mentionableExecutor);
         eb.setColor(Color.CYAN);
 
         eb.addField("Action Type", String.valueOf(ale.getType()), true);
         eb.addField("Target Type", String.valueOf(ale.getTargetType()), true);
-        eb.addField("🤖 Added a bot: ", "╰┈➤"+mentionableTarget, false);
+
+        eb.addField("Bot Added: ", "╰┈➤"+mentionableTarget, false);
 
         eb.setFooter("Audit Log Entry ID: "+ale.getId());
         eb.setTimestamp(ale.getTimeCreated());
 
-        MessageEmbed mb = eb.build();
-
         TextChannel sendingChannel = event.getGuild().getTextChannelById(channelIdToSendTo);
         if(sendingChannel!=null && sendingChannel.canTalk()) {
-            sendingChannel.sendMessageEmbeds(mb).queue();
+            sendingChannel.sendMessageEmbeds(eb.build()).queue();
         }
     }
 }
