fd78576c984738958451434391e331ad5a626d27
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-02-08 11:53:52 +0530

    refactor(channel-delete-event): improve channel delete event helper
    - Modernize audit log change processing using `forEach` and Java 14+ switch expressions
    - Streamline embed construction by directly building and sending the embed
    - Refine embed description and field names for clarity
    
    Signed-off-by: Egg-03 <111327101+eggy03@users.noreply.github.com>

--- src/main/java/io/github/eggy03/papertrail/bot/listeners/audit/helper/channel/ChannelDeleteEventHelper.java
+++ src/main/java/io/github/eggy03/papertrail/bot/listeners/audit/helper/channel/ChannelDeleteEventHelper.java
@@ -3,16 +3,13 @@ package io.github.eggy03.papertrail.bot.listeners.audit.helper.channel;
 import io.github.eggy03.papertrail.bot.commons.utilities.TypeResolver;
 import lombok.experimental.UtilityClass;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.audit.AuditLogChange;
 import net.dv8tion.jda.api.audit.AuditLogEntry;
-import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
 import net.dv8tion.jda.api.events.guild.GuildAuditLogEntryCreateEvent;
 
 import java.awt.Color;
-import java.util.Map;
 
 @UtilityClass
 public class ChannelDeleteEventHelper {
@@ -21,54 +18,54 @@ public class ChannelDeleteEventHelper {
 
         AuditLogEntry ale = event.getEntry();
 
+        User executor = ale.getJDA().getUserById(ale.getUserIdLong());
+        String mentionableExecutor = (executor != null ? executor.getAsMention() : ale.getUserId());
+
+        GuildChannel targetChannel = ale.getJDA().getGuildChannelById(ale.getTargetIdLong());
+
+        String mentionableTargetChannel = (targetChannel != null ? targetChannel.getAsMention() : ale.getTargetId()); // this will return only the ID cause the channel with the ID has been deleted
+
         EmbedBuilder eb = new EmbedBuilder();
         eb.setTitle("Audit Log Entry | Channel Delete Event");
 
-        User executor = ale.getJDA().getUserById(ale.getUserIdLong());
-        GuildChannel targetChannel = ale.getJDA().getGuildChannelById(ale.getTargetIdLong());
-
-        String mentionableExecutor = (executor != null ? executor.getAsMention() : ale.getUserId());
-        String mentionableTargetChannel = (targetChannel !=null ? targetChannel.getAsMention() : ale.getTargetId()); // this will return only the ID cause the channel with the ID has been deleted
-
-        eb.setDescription("👤 **By**: "+mentionableExecutor+"\nℹ️ The following channel was deleted");
+        eb.setDescription("ℹ️ The following channel was deleted by: " + mentionableExecutor);
         eb.setColor(Color.RED);
 
         eb.addField("Action Type", String.valueOf(ale.getType()), true);
         eb.addField("Target Type", String.valueOf(ale.getTargetType()), true);
 
+        ale.getChanges().forEach((changeKey, changeValue) -> {
 
-        for(Map.Entry<String, AuditLogChange> changes: ale.getChanges().entrySet()) {
+            Object oldValue = changeValue.getOldValue();
+            Object newValue = changeValue.getNewValue();
 
-            String change = changes.getKey();
-            Object oldValue = changes.getValue().getOldValue();
-            Object newValue = changes.getValue().getNewValue();
+            switch (changeKey) {
+                case "name" -> eb.addField("Name", "╰┈➤" + oldValue, false);
 
-            switch(change) {
-                case "name":
-                    eb.addField("🏷️ Name", "╰┈➤"+oldValue, false);
-                    break;
+                case "type" -> eb.addField("🗨️ Type", "╰┈➤" + TypeResolver.channelTypeResolver(oldValue), false);
 
-                case "type":
-                    eb.addField("🗨️ Type", "╰┈➤"+ TypeResolver.channelTypeResolver(oldValue), false);
-                    break;
+                case "user_limit", "rate_limit_per_user", "default_thread_rate_limit_per_user", "nsfw",
+                     "permission_overwrites", "video_quality_mode", "flags", "bitrate", "rtc_region", "template",
+                     "available_tags", "topic", "default_auto_archive_duration", "default_reaction_emoji" -> {
+                    // not strictly needed to be displayed
+                    // not all cases are available for all types of channels
+                }
 
-                case "user_limit", "rate_limit_per_user", "nsfw", "permission_overwrites", "video_quality_mode", "flags", "bitrate", "rtc_region":
-                    break;  // they're null during channel delete events
-
-                default:
-                    eb.addField(change, "from "+oldValue+" to "+newValue, false);
+                default -> {
+                    eb.addField(changeKey, "OLD_VALUE: " + oldValue, false);
+                    eb.addField(changeKey, "NEW_VALUE: " + newValue, false);
+                }
             }
-        }
+        });
 
-        eb.addField("🆔 Deleted Channel ID", "╰┈➤"+mentionableTargetChannel, false);
-        eb.setFooter("Audit Log Entry ID: "+ale.getId());
+        eb.addField("🆔 Deleted Channel ID", "╰┈➤" + mentionableTargetChannel, false);
+
+        eb.setFooter("Audit Log Entry ID: " + ale.getId());
         eb.setTimestamp(ale.getTimeCreated());
 
-        MessageEmbed mb = eb.build();
-
         TextChannel sendingChannel = event.getGuild().getTextChannelById(channelIdToSendTo);
-        if(sendingChannel!=null && sendingChannel.canTalk()) {
-            sendingChannel.sendMessageEmbeds(mb).queue();
+        if (sendingChannel != null && sendingChannel.canTalk()) {
+            sendingChannel.sendMessageEmbeds(eb.build()).queue();
         }
     }
 }
