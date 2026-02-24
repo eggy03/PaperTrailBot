b20656a8da5d0f040cd8f78c47e25e891ef5f06d
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-02-24 13:31:55 +0530

    refactor: remove javalin health check endpoint
    - remove `Javalin` dependency from `pom.xml`
    - remove `Javalin` health check endpoint implementation from `Start.java`
    - increment project version from `3.1.0` to `3.1.1`
    
    Signed-off-by: Egg-03 <111327101+eggy03@users.noreply.github.com>

--- src/main/java/io/github/eggy03/papertrail/bot/main/Start.java
+++ src/main/java/io/github/eggy03/papertrail/bot/main/Start.java
@@ -2,7 +2,6 @@ package io.github.eggy03.papertrail.bot.main;
 
 import io.github.eggy03.papertrail.bot.commons.utils.EnvConfig;
 import io.github.eggy03.papertrail.bot.listeners.misc.ActivityUpdateListener;
-import io.javalin.Javalin;
 import net.dv8tion.jda.api.sharding.ShardManager;
 
 import java.util.concurrent.ExecutorService;
@@ -13,24 +12,10 @@ import java.util.concurrent.Executors;
  */
 public class Start {
 
-    private static Javalin setHealthCheckEndpoint(int runningShards, int totalShards, int port) {
-        return Javalin
-                .create(config -> {
-                    config.concurrency.useVirtualThreads = true;
-                    config.routes.get("/health", ctx -> {
-                        if (runningShards != totalShards)
-                            ctx.status(503).result("Not Ready");
-                        else ctx.status(200).result("OK");
-                    });
-                })
-                .start(port);
-    }
-
     public static void main(String[] args) {
 
         // get env vars
         String token = EnvConfig.get("TOKEN");
-        int port = Integer.parseInt(EnvConfig.get("PORT"));
         int minShardId = Integer.parseInt(EnvConfig.get("MIN_SHARD_ID")); // min for this instance
         int maxShardId = Integer.parseInt(EnvConfig.get("MAX_SHARD_ID")); // max for this instance
         int totalShards = Integer.parseInt(EnvConfig.get("TOTAL_SHARDS"));
@@ -45,17 +30,9 @@ public class Start {
                 .applyPreBuildEventListeners(vThreadPool)
                 .applySharding(minShardId, maxShardId, totalShards)
                 .start();
+
         // post build listeners that require a fully built shard manager
         manager.addEventListener(new ActivityUpdateListener(manager));
-
-        // set a health check endpoint for containers
-        Javalin endpoint = setHealthCheckEndpoint(manager.getShardsRunning(), manager.getShardsTotal(), port);
-
-        // add shutdown hooks
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            endpoint.stop();
-            vThreadPool.shutdown();
-        }));
     }
 
 }
