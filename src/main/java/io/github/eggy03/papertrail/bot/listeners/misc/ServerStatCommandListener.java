73630647f7e10b9af9701553019809ef9f008c3e
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-02-26 00:37:43 +0530

    style(server-stats): improve server statistics embed formatting
    - Underline embed field titles using `MarkdownUtil.underline`
    - Quote block embed field values with `MarkdownUtil.quoteBlock`
    - Format member and bot count with newlines for readability
    - Refine embed title and description for clarity
    
    Signed-off-by: Egg-03 <111327101+eggy03@users.noreply.github.com>

--- src/main/java/io/github/eggy03/papertrail/bot/listeners/misc/ServerStatCommandListener.java
+++ src/main/java/io/github/eggy03/papertrail/bot/listeners/misc/ServerStatCommandListener.java
@@ -8,6 +8,7 @@ import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.utils.MarkdownUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.Color;
@@ -24,7 +25,7 @@ public class ServerStatCommandListener extends ListenerAdapter {
         int allUserCount = memberCache.size();
         int botCount = memberCache.stream().filter(member -> member.getUser().isBot()).toList().size();
 
-        return "Users: " + (allUserCount - botCount) + " Bots: " + botCount + " Total: " + allUserCount;
+        return "Users: " + (allUserCount - botCount) + "\nBots: " + botCount + "\nTotal: " + allUserCount;
     }
 
     @NotNull
@@ -76,28 +77,25 @@ public class ServerStatCommandListener extends ListenerAdapter {
         }
 
         EmbedBuilder eb = new EmbedBuilder();
-        eb.setTitle("📊 Server Statistics 📊");
-        eb.setDescription(guild.getName());
+        eb.setTitle("Server Statistics");
+        eb.setDescription("Statistics For: " + guild.getName());
         eb.setThumbnail(guild.getIconUrl());
         eb.setColor(Color.PINK);
 
-        eb.addField("Guild Owner", "╰┈➤" + getGuildOwner(guild), false);
-        eb.addField("Guild Created On", "╰┈➤" + getGuildCreationDate(guild), false);
-        eb.addField("Guild Vanity URL", "╰┈➤" + getGuildVanityUrl(guild), false);
-
-        eb.addField("Member Stats", "╰┈➤" + getMemberAndBotCount(guild), false);
-
-        eb.addField("Guild Boosters ", "╰┈➤" + getBoosters(guild), false);
-        eb.addField("Guild Boost Count", "╰┈➤" + guild.getBoostCount(), true);
-        eb.addField("Booster Role", "╰┈➤" + getBoosterRole(guild), true);
-        eb.addField("Boost Tier", "╰┈➤" + guild.getBoostTier().name(), true);
-
-        eb.addField("Locale", "╰┈➤" + guild.getLocale().getNativeName(), true);
-        eb.addField("Verification", "╰┈➤" + guild.getVerificationLevel().name(), true);
-        eb.addField("Roles", "╰┈➤" + guild.getRoles().size(), true);
-        eb.addField("Categories", "╰┈➤" + guild.getCategories().size(), true);
-        eb.addField("Text Channels", "╰┈➤" + guild.getTextChannels().size(), true);
-        eb.addField("Voice Channels", "╰┈➤" + guild.getVoiceChannels().size(), true);
+        eb.addField(MarkdownUtil.underline("Guild Owner"), MarkdownUtil.quoteBlock(getGuildOwner(guild)), false);
+        eb.addField(MarkdownUtil.underline("Guild Created On"), MarkdownUtil.quoteBlock(getGuildCreationDate(guild)), false);
+        eb.addField(MarkdownUtil.underline("Guild Vanity URL"), MarkdownUtil.quoteBlock(getGuildVanityUrl(guild)), false);
+        eb.addField(MarkdownUtil.underline("Member Stats"), MarkdownUtil.quoteBlock(getMemberAndBotCount(guild)), false);
+        eb.addField(MarkdownUtil.underline("Guild Boosters"), MarkdownUtil.quoteBlock(getBoosters(guild)), false);
+        eb.addField(MarkdownUtil.underline("Guild Boost Count"), MarkdownUtil.quoteBlock(String.valueOf(guild.getBoostCount())), true);
+        eb.addField(MarkdownUtil.underline("Booster Role"), MarkdownUtil.quoteBlock(getBoosterRole(guild)), true);
+        eb.addField(MarkdownUtil.underline("Boost Tier"), MarkdownUtil.quoteBlock(guild.getBoostTier().name()), true);
+        eb.addField(MarkdownUtil.underline("Locale"), MarkdownUtil.quoteBlock(guild.getLocale().getNativeName()), true);
+        eb.addField(MarkdownUtil.underline("Verification"), MarkdownUtil.quoteBlock(guild.getVerificationLevel().name()), true);
+        eb.addField(MarkdownUtil.underline("Roles"), MarkdownUtil.quoteBlock(String.valueOf(guild.getRoles().size())), true);
+        eb.addField(MarkdownUtil.underline("Categories"), MarkdownUtil.quoteBlock(String.valueOf(guild.getCategories().size())), true);
+        eb.addField(MarkdownUtil.underline("Text Channels"), MarkdownUtil.quoteBlock(String.valueOf(guild.getTextChannels().size())), true);
+        eb.addField(MarkdownUtil.underline("Voice Channels"), MarkdownUtil.quoteBlock(String.valueOf(guild.getVoiceChannels().size())), true);
 
         eb.setFooter("Requested By: " + getDataRequestingMember(event));
         eb.setTimestamp(Instant.now());
