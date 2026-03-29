41d7f06ababd1e6d6759785f6bd06ea8922c8df5
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-03-29 21:38:36 +0530

    refactor(onboard-utils): refactor channel list parsing to use streams
    Signed-off-by: Egg-03 <111327101+eggy03@users.noreply.github.com>

--- src/main/java/io/github/eggy03/papertrail/bot/listeners/auditlog/helper/onboarding/utils/OnboardingUtils.java
+++ src/main/java/io/github/eggy03/papertrail/bot/listeners/auditlog/helper/onboarding/utils/OnboardingUtils.java
@@ -1,5 +1,6 @@
 package io.github.eggy03.papertrail.bot.listeners.auditlog.helper.onboarding.utils;
 
+import io.github.eggy03.papertrail.bot.commons.utils.NumberParseUtils;
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,8 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @UtilityClass
 @Slf4j
@@ -36,20 +39,19 @@ public class OnboardingUtils {
 
     @NotNull
     public static String resolveChannelsFromList(@NonNull Guild guild, @Nullable Object value) {
-        if (value == null) {
-            log.debug("channel list was null");
+
+        if (!(value instanceof List<?> channelIdList) || channelIdList.isEmpty())
             return FALLBACK_STRING;
-        }
 
-        StringBuilder sb = new StringBuilder();
-        if (value instanceof List<?> channelIdList) {
-            channelIdList.forEach(channelId -> {
-                GuildChannel channel = guild.getGuildChannelById((String) channelId);
-                sb.append(channel != null ? channel.getAsMention() : channelId).append(" ");
-            });
-        }
+        return channelIdList
+                .stream()
+                .map(NumberParseUtils::parseLong)
+                .filter(Objects::nonNull)
+                .map(guild::getGuildChannelById)
+                .filter(Objects::nonNull)
+                .map(GuildChannel::getAsMention)
+                .collect(Collectors.joining(" "))
+                .trim();
 
-        log.debug("channel list was not an instance of a list, value: {}", value);
-        return sb.toString().trim();
     }
 }
