744dbf91e2ed895b3cb853e5b34ebd19ac2188f7
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-05-31 12:23:29 +0530

    refactor(onboardingevents): migrate onboarding helpers to two handlers
    Signed-off-by: Egg-03 <111327101+eggy03@users.noreply.github.com>

--- src/main/java/io/github/eggy03/papertrail/bot/utils/auditlog/OnboardingUtils.java
+++ src/main/java/io/github/eggy03/papertrail/bot/utils/auditlog/OnboardingUtils.java
@@ -1,4 +1,4 @@
-package io.github.eggy03.papertrail.bot.service.auditlog.onboarding.utils;
+package io.github.eggy03.papertrail.bot.utils.auditlog;
 
 import io.github.eggy03.papertrail.bot.utils.NumberParseUtils;
 import lombok.NonNull;
