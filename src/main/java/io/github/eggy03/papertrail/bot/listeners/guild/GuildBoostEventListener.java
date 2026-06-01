63e1ac9481b6f502f51464bd2a8057b48bfe49bc
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-06-01 10:19:26 +0530

    refactor(guildboostevents): consolidate boost event helpers into single handler
    Signed-off-by: Egg-03 <111327101+eggy03@users.noreply.github.com>

--- src/main/java/io/github/eggy03/papertrail/bot/listeners/guild/GuildBoostEventListener.java
+++ src/main/java/io/github/eggy03/papertrail/bot/listeners/guild/GuildBoostEventListener.java
@@ -1,60 +1,36 @@
 package io.github.eggy03.papertrail.bot.listeners.guild;
 
-import io.github.eggy03.papertrail.bot.handlers.guild.GuildMemberUpdateBoostTimeEventHelper;
-import io.github.eggy03.papertrail.bot.handlers.guild.GuildUpdateBoostCountEventHelper;
-import io.github.eggy03.papertrail.bot.handlers.guild.GuildUpdateBoostTierEventHelper;
-import io.github.eggy03.papertrail.sdk.client.AuditLogRegistrationClient;
-import io.github.eggy03.papertrail.sdk.entity.AuditLogRegistrationEntity;
+import io.github.eggy03.papertrail.bot.handlers.guild.GuildBoostEventHandler;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateBoostTimeEvent;
 import net.dv8tion.jda.api.events.guild.update.GuildUpdateBoostCountEvent;
 import net.dv8tion.jda.api.events.guild.update.GuildUpdateBoostTierEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-import java.util.Optional;
-import java.util.concurrent.Executor;
-
-// Experimental
-@RequiredArgsConstructor
+@ApplicationScoped
 public final class GuildBoostEventListener extends ListenerAdapter {
 
-    @NonNull
-    private final AuditLogRegistrationClient client;
+    private final @NonNull GuildBoostEventHandler handler;
 
-    @NonNull
-    private final Executor vThreadPool;
+    @Inject
+    public GuildBoostEventListener(@NonNull GuildBoostEventHandler handler) {
+        this.handler = handler;
+    }
 
     @Override
     public void onGuildUpdateBoostTier(@NonNull GuildUpdateBoostTierEvent event) {
-
-        vThreadPool.execute(() -> {
-            Optional<AuditLogRegistrationEntity> response = client.getRegisteredGuild(event.getGuild().getId());
-            response.ifPresent(success ->
-                    GuildUpdateBoostTierEventHelper.format(event, success.getChannelId())
-            );
-        });
+        handler.handleUpdateBoostTier(event);
     }
 
     @Override
     public void onGuildUpdateBoostCount(@NonNull GuildUpdateBoostCountEvent event) {
-
-        vThreadPool.execute(() -> {
-            Optional<AuditLogRegistrationEntity> response = client.getRegisteredGuild(event.getGuild().getId());
-            response.ifPresent(success ->
-                    GuildUpdateBoostCountEventHelper.format(event, success.getChannelId())
-            );
-        });
+        handler.handleUpdateBoostCount(event);
     }
 
     @Override
     public void onGuildMemberUpdateBoostTime(@NonNull GuildMemberUpdateBoostTimeEvent event) {
-
-        vThreadPool.execute(() -> {
-            Optional<AuditLogRegistrationEntity> response = client.getRegisteredGuild(event.getGuild().getId());
-            response.ifPresent(success ->
-                    GuildMemberUpdateBoostTimeEventHelper.format(event, success.getChannelId())
-            );
-        });
+        handler.handleMemberUpdateBoostTime(event);
     }
 }
