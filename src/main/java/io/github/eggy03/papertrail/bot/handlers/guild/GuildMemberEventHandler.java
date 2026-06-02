547dcf3623a4c9a4e31541e7e705d0908951db65
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-06-02 19:53:01 +0530

    refactor: make GuildMemberEventHandler final
    Signed-off-by: Egg-03 <111327101+eggy03@users.noreply.github.com>

--- src/main/java/io/github/eggy03/papertrail/bot/handlers/guild/GuildMemberEventHandler.java
+++ src/main/java/io/github/eggy03/papertrail/bot/handlers/guild/GuildMemberEventHandler.java
@@ -24,7 +24,7 @@ import java.time.Instant;
 
 @ApplicationScoped
 @Slf4j
-public class GuildMemberEventHandler {
+public final class GuildMemberEventHandler {
 
     private final @NonNull AuditLogRegistrationClient client;
 
