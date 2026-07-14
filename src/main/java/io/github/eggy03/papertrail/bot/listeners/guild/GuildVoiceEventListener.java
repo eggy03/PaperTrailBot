65e8bcef7db892efa027625e5e72225416cdd354
Egg-03 <111327101+eggy03@users.noreply.github.com>
2026-07-14 09:55:40 +0530

    refactor: move handler classes to `service.handlers` package
    Signed-off-by: Egg-03 <111327101+eggy03@users.noreply.github.com>

--- src/main/java/io/github/eggy03/papertrail/bot/listeners/guild/GuildVoiceEventListener.java
+++ src/main/java/io/github/eggy03/papertrail/bot/listeners/guild/GuildVoiceEventListener.java
@@ -1,7 +1,7 @@
 package io.github.eggy03.papertrail.bot.listeners.guild;
 
 import io.github.eggy03.papertrail.bot.annotations.VirtualThreadFactory;
-import io.github.eggy03.papertrail.bot.handlers.guild.GuildVoiceEventHandler;
+import io.github.eggy03.papertrail.bot.service.handlers.guild.GuildVoiceEventHandler;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 import lombok.NonNull;
